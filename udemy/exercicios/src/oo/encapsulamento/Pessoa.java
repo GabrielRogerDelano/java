c9e11064cd1237bfed0590156d0336bc04bdbbc7
Gabriel Roger <122801172+GabrielRogerDelano@users.noreply.github.com>
2023-10-03 19:01:06 -0300

    getter e setter

--- udemy/exercicios/src/oo/encapsulamento/Pessoa.java
+++ udemy/exercicios/src/oo/encapsulamento/Pessoa.java
@@ -2,11 +2,35 @@ package oo.encapsulamento;
 
 public class Pessoa {
 	private int idade;
+	String nome;
+	String sobreNome;
 	
-	public Pessoa(int idade) {
+	public Pessoa(String nome, String sobreNome, int idade) {
+		setNome(nome);
+		setSobreNome(sobreNome);
 		setIdade(idade);
 	}
 	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobreNome() {
+		return sobreNome;
+	}
+
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	
+	public String getNomeCompleto() {
+		return getNome() + " " + getSobreNome();
+	}
+
 	public int getIdade() {
 		return this.idade;
 	}
--- udemy/exercicios/src/oo/encapsulamento/PessoaTeste.java
+++ udemy/exercicios/src/oo/encapsulamento/PessoaTeste.java
@@ -2,9 +2,10 @@ package oo.encapsulamento;
 
 public class PessoaTeste {
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(-56);
+		Pessoa p1 = new Pessoa("Paulo", "Mussi", -56);
 		p1.setIdade(40);
 		
 		System.out.println(p1.getIdade());
+		System.out.println(p1.getNomeCompleto());
 	}
 }
