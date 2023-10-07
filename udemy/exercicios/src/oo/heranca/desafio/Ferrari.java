5cf17c418edce4fb292eb45a6caaef07bb44b109
Gabriel Roger <122801172+GabrielRogerDelano@users.noreply.github.com>
2023-10-06 22:50:32 -0300

    .

--- udemy/exercicios/src/oo/heranca/desafio/Ferrari.java
+++ udemy/exercicios/src/oo/heranca/desafio/Ferrari.java
@@ -2,12 +2,19 @@ package oo.heranca.desafio;
 
 import oo.heranca.teste.Carro;
 
-public class Ferrari extends Carro{
+public class Ferrari extends Carro implements Esportivo{
 	
 	public Ferrari() {
 		this(300);
 	}
+	
+	public void ligarTurbo() {
+		delta = 30;
+	}
 
+	public void desligarTurbo() {
+		delta = 15;
+	}
 	public Ferrari(int velocidadeMaxima) {
 		super(velocidadeMaxima);
 		delta = 15;
