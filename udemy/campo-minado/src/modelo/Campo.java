9f9f4b6ca740b10c9605776a70a1b1e48b299d8a
GabrielRogerDelano <gabrielroger932@gmail.com>
2025-07-31 22:32:23 -0300

    iniciando jogo de campo minado

---
+++ udemy/campo-minado/src/modelo/Campo.java
@@ -0,0 +1,24 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Campo {
+	private final int linha;
+	private final int coluna;
+	
+	private boolean minado = false;
+	private boolean aberto = false;
+	private boolean marcado = false;
+	
+	private List<Campo> vizinho = new ArrayList<>();
+	
+	Campo(int linha, int coluna){
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
+	boolean AdicionarVizinho() {
+		
+	}
+}
