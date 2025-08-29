9f9c499041abeb17d66e1920fa4bcb6191e15a8b
GabrielRogerDelano <gabrielroger932@gmail.com>
2025-08-29 10:44:36 -0300

    estrutura chave valor com generics

---
+++ exercicios/src/generics/ParesTeste.java
@@ -0,0 +1,16 @@
+package generics;
+
+
+public class ParesTeste {
+	public static void main(String[] args) {
+		Pares<Integer, String> resultadoConcurso= new Pares<>();
+		
+		resultadoConcurso.adicionar(1, "Angela");
+		resultadoConcurso.adicionar(2, "Carlos");
+		resultadoConcurso.adicionar(3, "Caio");
+		resultadoConcurso.adicionar(4, "Yumi");
+		resultadoConcurso.adicionar(2, "Ursula");
+		
+		System.out.println(resultadoConcurso.getValor(2));
+	}
+}
